f8167c6d7f96b532514ee564ce742711a660eed6
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-05 00:18:28 +0530

    Register authentication route file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         then: function (): void {
             Route::middleware('web')
                 ->group(base_path('routes/search.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/auth.php'));
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
