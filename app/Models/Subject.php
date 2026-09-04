24eb0f6898c40bc8447dbc9b42776626975070dd
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-05 04:57:14 +0530

    Add Subject model

---
+++ app/Models/Subject.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'short_description',
+        'description',
+        'sort_order',
+        'is_published',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'sort_order' => 'integer',
+            'is_published' => 'boolean',
+        ];
+    }
+
+    public function scopePublished(Builder $query): Builder
+    {
+        return $query->where('is_published', true);
+    }
+}
