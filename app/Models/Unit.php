a61732a0d862f9f16909d7f1f3bd3e4fa5d54914
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-05 05:11:51 +0530

    Connect units to lessons

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Unit extends Model
 {
@@ -34,6 +35,13 @@ class Unit extends Model
     public function course(): BelongsTo
     {
         return $this->belongsTo(Course::class);
+    }
+
+    public function lessons(): HasMany
+    {
+        return $this->hasMany(Lesson::class)
+            ->orderBy('sort_order')
+            ->orderBy('title');
     }
 
     public function scopePublished(Builder $query): Builder
