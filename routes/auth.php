320273238d3dacb37c364f60565097a471a3ce70
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-05 04:33:43 +0530

    Add authenticated My Learning route

--- routes/auth.php
+++ routes/auth.php
@@ -17,6 +17,7 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::view('/my-learning', 'my-learning')->name('my-learning');
 
     Route::post('/logout', [AuthController::class, 'logout'])
         ->name('logout');
