9066a2342cbbb30e36d18f1d128174d4e4e5b9fc
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-05 06:15:36 +0530

    Route public search through database search controller

--- routes/search.php
+++ routes/search.php
@@ -1,6 +1,7 @@
 <?php
 
+use App\Http\Controllers\PublicSearchController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/search', 'pages.search')
+Route::get('/search', PublicSearchController::class)
     ->name('search');
