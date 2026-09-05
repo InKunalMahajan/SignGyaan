81bb13100dd8f964ddd8d1fccfdfffdfcd57ba8e
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-05 16:16:12 +0530

    Add course publishing checklist route

--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\CourseBuilderController;
 use App\Http\Controllers\Admin\CourseBuilderDuplicateController;
 use App\Http\Controllers\Admin\CourseController;
 use App\Http\Controllers\Admin\CoursePreviewController;
+use App\Http\Controllers\Admin\CoursePublishingChecklistController;
 use App\Http\Controllers\Admin\LessonController;
 use App\Http\Controllers\Admin\MediaController;
 use App\Http\Controllers\Admin\MediaPickerController;
@@ -25,6 +26,7 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(fun
 
     Route::get('/courses/{course}/builder', CourseBuilderController::class)->name('courses.builder');
     Route::get('/courses/{course}/preview', CoursePreviewController::class)->name('courses.preview');
+    Route::get('/courses/{course}/publishing-checklist', CoursePublishingChecklistController::class)->name('courses.publishing-checklist');
     Route::post('/courses/{course}/builder/quick-lesson', [CourseBuilderController::class, 'quickLesson'])->middleware('throttle:30,1')->name('courses.builder.quick-lesson');
     Route::post('/courses/{course}/builder/reorder', [CourseBuilderController::class, 'reorder'])->middleware('throttle:60,1')->name('courses.builder.reorder');
     Route::post('/courses/{course}/builder/duplicate', [CourseBuilderDuplicateController::class, 'course'])->middleware('throttle:10,1')->name('courses.builder.duplicate-course');
