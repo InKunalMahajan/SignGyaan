5c15330d90fadebe6e08642e14d9c8b218067906
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-05 06:27:24 +0530

    Constrain public catalogue slugs

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,14 @@ Route::get('/subjects', [PublicCatalogController::class, 'subjects'])
     ->name('subjects');
 
 Route::get('/subjects/{subject}', [PublicCatalogController::class, 'subject'])
+    ->where('subject', '[a-z0-9]+(?:-[a-z0-9]+)*')
     ->name('subjects.show');
 
 Route::get('/subjects/{subject}/courses/{course}', [PublicCatalogController::class, 'course'])
+    ->where([
+        'subject' => '[a-z0-9]+(?:-[a-z0-9]+)*',
+        'course' => '[a-z0-9]+(?:-[a-z0-9]+)*',
+    ])
     ->name('courses.show');
 
 Route::get('/explore', [PublicCatalogController::class, 'explore'])
