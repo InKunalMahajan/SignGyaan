244a5379f9bc8ae8c1b673e3f27fa28d3d289c83
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-05 18:55:29 +0530

    Add learner history route

--- routes/auth.php
+++ routes/auth.php
@@ -21,6 +21,8 @@ Route::middleware('auth')->group(function () {
         ->name('my-courses.progress');
     Route::get('/assessment-performance', [LearningController::class, 'assessmentPerformance'])
         ->name('assessment-performance');
+    Route::get('/learning-history', [LearningController::class, 'learningHistory'])
+        ->name('learning-history');
 
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile');
     Route::patch('/profile', [ProfileController::class, 'update'])->middleware('throttle:20,1')->name('profile.update');
