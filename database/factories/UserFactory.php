3c2322df7d7aa996d9ce496db82cebfbf7745b84
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-05 21:38:20 +0530

    Align user factory with management foundation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,8 +29,32 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => User::ROLE_LEARNER,
+            'status' => User::STATUS_ACTIVE,
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => User::ROLE_ADMIN,
+        ]);
+    }
+
+    public function suspended(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'status' => User::STATUS_SUSPENDED,
+            'suspended_at' => now(),
+        ]);
+    }
+
+    public function disabled(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'status' => User::STATUS_DISABLED,
+        ]);
     }
 
     /**
