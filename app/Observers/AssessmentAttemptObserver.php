5ea1e361e348fa31d61bda5a7e03f5c5a40d468a
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-05 20:16:58 +0530

    Keep assessment notifications transaction-safe

--- app/Observers/AssessmentAttemptObserver.php
+++ app/Observers/AssessmentAttemptObserver.php
@@ -4,9 +4,8 @@ namespace App\Observers;
 
 use App\Models\AssessmentAttempt;
 use App\Services\AssessmentNotificationService;
-use Illuminate\Contracts\Events\ShouldHandleEventsAfterCommit;
 
-class AssessmentAttemptObserver implements ShouldHandleEventsAfterCommit
+class AssessmentAttemptObserver
 {
     public function updated(AssessmentAttempt $attempt): void
     {
