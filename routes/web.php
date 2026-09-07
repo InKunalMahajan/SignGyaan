2159ac9621f27e49029d2ed4194ef3dfa2a4c96c
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-08 00:17:43 +0530

    Add dashboard preview routes for five roles

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'show'])
+    ->name('dashboard');
+
+Route::get('/dashboard/{role}', [DashboardController::class, 'show'])
+    ->whereIn('role', ['learner', 'parents', 'teacher', 'admin', 'guest'])
+    ->name('dashboard.role');
