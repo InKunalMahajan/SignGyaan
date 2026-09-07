ba2ba12db91eb10cbda0b72aff79cd4173eecd67
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-08 00:24:38 +0530

    Add user role support

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,8 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    public const ROLES = ['learner', 'parents', 'teacher', 'admin'];
+
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +23,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'role',
         'password',
     ];
 
@@ -33,6 +36,11 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function hasRole(string $role): bool
+    {
+        return $this->role === $role;
+    }
 
     /**
      * Get the attributes that should be cast.
